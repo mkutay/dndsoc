c045e80957b0cf679e64afcdee7e3cd04108f867
Mehmet Kutay Bozkurt <me@mkutay.dev>
2025-07-18 01:21:44 +0300

    refactor: improve privacy policy and terms of service

--- src/app/privacy-policy/page.tsx
+++ src/app/privacy-policy/page.tsx
@@ -1,39 +1,41 @@
-import { EB_Garamond } from "next/font/google";
-import Link from "next/link";
+import type { Metadata } from "next";
 
 import { TypographyH1, TypographyH2, TypographyH3 } from "@/components/typography/headings";
-import { TypographyParagraph } from "@/components/typography/paragraph";
+import { TypographyLink, TypographyParagraph } from "@/components/typography/paragraph";
+import { TypographyList } from "@/components/typography/list";
 
-const garamond = EB_Garamond({
-  subsets: ["latin"],
-  display: "swap",
-});
+export const metadata: Metadata = {
+  title: "KCL D&D Society App Privacy Policy",
+  description: "Privacy policy for the KCL D&D Society app, detailing how we handle your personal data.",
+  openGraph: {
+    title: "KCL D&D Society App Privacy Policy",
+    description: "Privacy policy for the KCL D&D Society app, detailing how we handle your personal data.",
+  },
+};
 
-export default async function Page() {
+export default function Page() {
   return (
     <div className="flex flex-col w-full mx-auto lg:max-w-6xl max-w-prose lg:my-12 mt-6 mb-12 px-4">
-      <div className={garamond.className}>
-        <TypographyH1 className={garamond.className}>KCL D&D General Privacy Statement</TypographyH1>
-        <TypographyParagraph> </TypographyParagraph>
+      <div className="max-w-2xl">
+        <TypographyH1>KCL DnD Society App Privacy Policy</TypographyH1>
 
-        <TypographyH2 className={garamond.className}>KCL D&D Privacy Statement</TypographyH2>
+        <TypographyH2 className="mt-8">KCL D&D Privacy Statement</TypographyH2>
         <TypographyParagraph>
-          Here is where we tell you how we handle your &ldquo;Personal Data&rdquo;, which is information that can be
+          Here is where we tell you how we handle your &ldquo;Personal Data,&rdquo; which is information that can be
           directly or indirectly linked to you. It applies to the Personal Data that KCL D&D (&ldquo;Data
-          Controller&rdquo; or &ldquo;We&rdquo;) processes when you interact with our &ldquo;Services&rdquo;.
+          Controller&rdquo; or &ldquo;We&rdquo;) processes when you interact with our &ldquo;Services.&rdquo;
         </TypographyParagraph>
-        <TypographyParagraph> </TypographyParagraph>
 
-        <TypographyH2 className={garamond.className}>Personal Data We Collect</TypographyH2>
+        <TypographyH2 className="mt-8">Personal Data We Collect</TypographyH2>
 
-        <TypographyH3 className={garamond.className}>From You</TypographyH3>
-        <ul>
+        <TypographyH3 className="mt-6">From You</TypographyH3>
+        <TypographyList>
           <li>
-            Account Data: We collect information when you open an account like your Name, Username, Email, KNumber, and
+            Account Data: We collect information when you open an account like your name, username, email, K-number, and
             password.
           </li>
           <li>
-            User Content and Files: When you use our Services, We collect Personal Data included as part of the
+            User Content and Files: When you use our Services, we collect Personal Data included as part of the
             information you provided like character sheet details, images, files, etc.
           </li>
           <li>Feedback Data: This consists of information you submit through surveys and reviews.</li>
@@ -44,54 +46,61 @@ export default async function Page() {
           <li>
             Support Data: When you seek customer support, we collect details like code, text, or multimedia files.
           </li>
-        </ul>
-        <TypographyParagraph> </TypographyParagraph>
+        </TypographyList>
 
-        <TypographyH3 className={garamond.className}>Automatically</TypographyH3>
-        <ul>
+        <TypographyH3 className="mt-6">Automatically</TypographyH3>
+        <TypographyList>
           <li>
             Essential Cookies and Similar Tracking Technologies: We reserve the right to use cookies and similar
             technologies to provide essential functionality.
           </li>
-        </ul>
-        <TypographyParagraph> </TypographyParagraph>
+        </TypographyList>
 
-        <TypographyH2 className={garamond.className}>
+        <TypographyH2 className="mt-8">Third Party Services</TypographyH2>
+
+        <TypographyParagraph>
+          There are two third party services we use to help us operate our Services:{" "}
+          <TypographyLink href="https://hetzner.com">Hetzner</TypographyLink> (for hosting) and{" "}
+          <TypographyLink href="https://cloudflare.com">Cloudflare</TypographyLink> (for DNS resolving, DOS protection,
+          and domain services). These third parties may have access to your Personal Data only to perform specific tasks
+          on our behalf and are obligated not to disclose or use it for any other purpose. Apart from them, we do not
+          use any third-party services, as we host our Services ourselves.
+        </TypographyParagraph>
+
+        <TypographyH2 className="mt-8">
           Lawful Bases for Processing Personal Data (Applicable to EEA and UK End Users)
         </TypographyH2>
         <TypographyParagraph>
           We processes Personal Data in compliance with the GDPR, ensuring a lawful basis for each processing activity.
         </TypographyParagraph>
-        <TypographyParagraph> </TypographyParagraph>
 
-        <TypographyH1 className={garamond.className}>Your Privacy Rights</TypographyH1>
+        <TypographyH2 className="mt-8">Your Privacy Rights</TypographyH2>
         <TypographyParagraph>
           Depending on your residence location, you may have specific legal rights regarding your Personal Data:
         </TypographyParagraph>
-        <ul>
-          <li>The right to access the data collected about you</li>
+        <TypographyList>
+          <li>The right to access the data collected about you.</li>
           <li>
             The right to request detailed information about the specific types of Personal Data we&apos;ve collected
-            over the past 12 months, including data disclosed for business purposes
+            over the past 12 months, including data disclosed for business purposes.
           </li>
-          <li>The right to rectify or update inaccurate or incomplete Personal Data under certain circumstances</li>
-          <li>The right to erase or limit the processing of your Personal Data under specific conditions</li>
-          <li>The right to object to the processing of your Personal Data, as allowed by applicable law</li>
-          <li>The right to withdraw consent, where processing is based on your consent</li>
+          <li>The right to rectify or update inaccurate or incomplete Personal Data under certain circumstances.</li>
+          <li>The right to erase or limit the processing of your Personal Data under specific conditions.</li>
+          <li>The right to object to the processing of your Personal Data, as allowed by applicable law.</li>
+          <li>The right to withdraw consent, where processing is based on your consent.</li>
           <li>
             The right to receive your collected Personal Data in a structured, commonly used, and machine-readable
-            format to facilitate its transfer to another company, where technically feasible
+            format to facilitate its transfer to another company, where technically feasible.
           </li>
-        </ul>
+        </TypographyList>
         <TypographyParagraph>
           To exercise these rights, please send a message to us at{" "}
-          <Link href="mailto:dndsockcl@gmail.com">dndsockcl@gmail.com</Link> and follow the instructions provided. To
-          verify your identity for security, we may request extra information before addressing your data-related
-          request.
+          <TypographyLink href="mailto:dndsockcl@gmail.com">dndsockcl@gmail.com</TypographyLink> and follow the
+          instructions provided. To verify your identity for security, we may request extra information before
+          addressing your data-related request.
         </TypographyParagraph>
-        <TypographyParagraph> </TypographyParagraph>
 
-        <TypographyH1 className={garamond.className}>TLDR:</TypographyH1>
+        <TypographyH2 className="mt-8">TLDR</TypographyH2>
         <TypographyParagraph>
           We collect data which you provide us in order to be able to make our services functional. We made the hard
           decision and decided not to sell any of it to advertisers (or hordes of orc). We hold your data until you
--- src/app/terms-of-service/page.tsx
+++ src/app/terms-of-service/page.tsx
@@ -1,27 +1,35 @@
-import { EB_Garamond } from "next/font/google";
+import type { Metadata } from "next";
 
-import { TypographyH1, TypographyH2 } from "@/components/typography/headings";
-import { TypographyParagraph } from "@/components/typography/paragraph";
+import { TypographyH1, TypographyH2, TypographyH3 } from "@/components/typography/headings";
+import { TypographyParagraph, TypographySmall } from "@/components/typography/paragraph";
+import { TypographyList } from "@/components/typography/list";
 
-const garamond = EB_Garamond({
-  subsets: ["latin"],
-  display: "swap",
-});
+export const metadata: Metadata = {
+  title: "Terms of Service",
+  description: "Terms of service for the KCL D&D Society app, outlining user responsibilities and rights.",
+  openGraph: {
+    title: "Terms of Service",
+    description: "Terms of service for the KCL D&D Society app, outlining user responsibilities and rights.",
+  },
+};
 
-export default async function Page() {
+export default function Page() {
   return (
     <div className="flex flex-col w-full mx-auto lg:max-w-6xl max-w-prose lg:my-12 mt-6 mb-12 px-4">
-      <div className={garamond.className}>
-        <TypographyH1 className={garamond.className}>Terms of Service</TypographyH1>
+      <div className="max-w-2xl">
+        <TypographyH1>Terms of Service</TypographyH1>
         <TypographyParagraph>
-          &ldquo;We&rdquo;, &ldquo;Us&rdquo;, &ldquo;Our&rdquo; are in reference to KCL DND. &ldquo;You&rdquo;,
-          &ldquo;Your&rdquo; are in reference to the user.
+          &ldquo;We&rdquo;, &ldquo;Us&rdquo;, &ldquo;Our&rdquo; are in reference to KCL DnD Society. &ldquo;You&rdquo;,
+          &ldquo;Your&rdquo; are in reference to the user. &ldquo;Agreement&rdquo; is in reference to these terms of
+          service.
         </TypographyParagraph>
-        <TypographyParagraph>
-          By accessing Our services You accept and agree to the following terms and conditions: We give no warranty as
-          to the accuracy of the information or guidance for any specific purpose. The information and guidance
-          contained on the website does not constitute legal or professional advice.
+        <TypographyParagraph className="mb-3">
+          By accessing Our services You accept and agree to the following terms and conditions:
         </TypographyParagraph>
+        <TypographySmall>
+          We give no warranty as to the accuracy of the information or guidance for any specific purpose. The
+          information and guidance contained on the website does not constitute legal or professional advice.
+        </TypographySmall>
         <TypographyParagraph>
           All implied warranties and conditions are excluded, to the maximum extent permitted by law. We are in no way
           responsible or liable for any damage or loss whatsoever arising from viewing or downloading information from
@@ -33,34 +41,31 @@ export default async function Page() {
           therefore recommend that you check these each time you revisit the site. You must not use our services, and/or
           any interactive facilities which are available through it to:
         </TypographyParagraph>
-        <ul>
-          <li>commit or encourage unlawful acts or in breach of these terms of use;</li>
-          <li>misrepresent your identity;</li>
-          <li>hack, or attempt to hack, any part of the Our Services or any system used to run them;</li>
-          <li>post material that is obscene, offensive, or otherwise unlawful;</li>
+        <TypographyList>
+          <li>Commit or encourage unlawful acts or in breach of these terms of use;</li>
+          <li>Misrepresent your identity;</li>
+          <li>Hack, or attempt to hack, any part of the Our Services or any system used to run them;</li>
+          <li>Post material that is obscene, offensive, or otherwise unlawful;</li>
           <li>
-            technically harm the website and/or its systems (including, without limitation, computer viruses or
+            Technically harm the website and/or its systems (including, without limitation, computer viruses or
             malicious software or harmful data).
           </li>
-        </ul>
+        </TypographyList>
         <TypographyParagraph>
           While We try to make this website available 24 hours a day, We reserve the right to suspend or withdraw
           access, without notice, to either the whole or part of Our Services, for any reason.
         </TypographyParagraph>
         <TypographyParagraph>We reserve the right to ban Your account for any reason.</TypographyParagraph>
-        <TypographyParagraph> </TypographyParagraph>
 
-        <TypographyH1 className={garamond.className}>Disclaimer</TypographyH1>
-        <TypographyParagraph> </TypographyParagraph>
+        <TypographyH2 className="mt-8">Disclaimer</TypographyH2>
 
-        <TypographyH2 className={garamond.className}>No liability to third parties</TypographyH2>
+        <TypographyH3 className="mt-6">No liability to third parties</TypographyH3>
         <TypographyParagraph>
           Our dealings and contracts with Our users do not create a contract or other legally binding relationship
           between Us and anyone else, for example parents, guardians or friends.
         </TypographyParagraph>
-        <TypographyParagraph> </TypographyParagraph>
 
-        <TypographyH2 className={garamond.className}>Complaints</TypographyH2>
+        <TypographyH3 className="mt-6">Complaints</TypographyH3>
         <TypographyParagraph>
           Any complaint made about any material available from Our Services or information relating to the Us contained
           on external Internet sites will be addressed as soon as possible. These terms of service represent the entire
@@ -69,16 +74,14 @@ export default async function Page() {
           be subject to conditions and/or disclaimers as indicated. English law governs these terms and your use of this
           website and you submit to the non-exclusive jurisdiction of the English courts.
         </TypographyParagraph>
-        <TypographyParagraph> </TypographyParagraph>
 
-        <TypographyH2 className={garamond.className}>Failure to Exercise Rights</TypographyH2>
+        <TypographyH3 className="mt-6">Failure to Exercise Rights</TypographyH3>
         <TypographyParagraph>
           The failure of either party to exercise any rights under these terms and conditions shall not be deemed to be
           a waiver of that right.
         </TypographyParagraph>
-        <TypographyParagraph> </TypographyParagraph>
 
-        <TypographyH2 className={garamond.className}>If Any Provisions are Invalid</TypographyH2>
+        <TypographyH3 className="mt-6">If Any Provisions are Invalid</TypographyH3>
         <TypographyParagraph>
           If a court holds any provision of this Agreement to be illegal, invalid, or unenforceable, the remaining
           provisions will remain in full force and effect and the parties will amend the Agreement to give effect to the
