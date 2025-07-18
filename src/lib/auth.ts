eb785a09e6dbf40819e8a38e7ca84644c5a17e80
Mehmet Kutay Bozkurt <me@mkutay.dev>
2025-07-18 17:17:20 +0300

    fix: change origin url for use with preview deployments

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -143,6 +143,7 @@ export const signUpUser = ({
               username,
               knumber,
               name,
+              siteUrl: origin, // Here, for preview and production deployments
             },
           },
         }),
