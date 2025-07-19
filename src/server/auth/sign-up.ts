c5befda9bbbd485b2e82f99c4fbd6b69da4db65c
Mehmet Kutay Bozkurt <me@mkutay.dev>
2025-07-20 00:40:05 +0300

    refactor: improve uniqueness checking

--- src/server/auth/sign-up.ts
+++ src/server/auth/sign-up.ts
@@ -1,17 +1,16 @@
 "use server";
 
-import { errAsync, fromSafePromise, okAsync } from "neverthrow";
+import { errAsync, okAsync } from "neverthrow";
 import { z } from "zod";
 
 import { associatesSignUpFormSchema, signUpFormSchema } from "@/config/auth-schemas";
 import { sendAssociatesSignUpRequest } from "@/lib/associates-requests";
+import { sendNewAssociatesSignUpRequestToAdmin } from "@/lib/email";
 import { resultAsyncToActionResult } from "@/types/error-typing";
 import { resendConfirmationEmail, signUpUser } from "@/lib/auth";
 import { parseSchema } from "@/utils/parse-schema";
-import { runServiceQuery } from "@/utils/supabase-run";
+import { runQuery, runServiceQuery } from "@/utils/supabase-run";
 import { getOrigin } from "@/lib/origin";
-import { sendNewAssociatesSignUpRequestToAdmin } from "@/lib/email";
-import { createServiceClient } from "@/utils/supabase/server";
 
 export const signUpAction = async (values: z.infer<typeof signUpFormSchema>) =>
   resultAsyncToActionResult(
@@ -41,13 +40,16 @@ type CheckUniquenessError = {
 };
 
 const checkUniqueness = (values: z.infer<typeof signUpFormSchema>) =>
-  runServiceQuery((supabase) =>
-    supabase.from("users").select("*").or(`username.eq.${values.username}, knumber.eq.${values.knumber}`),
+  runQuery((supabase) =>
+    supabase
+      .from("users")
+      .select("*")
+      .or(`username.eq.${values.username}, knumber.eq.${values.knumber}, email.eq.${values.email}`),
   )
     .andThen((data) =>
       data.length !== 0
         ? errAsync({
-            message: "Username or K-Number already exists.",
+            message: "Username, K-Number, or Email already exists.",
             code: "UNIQUE_VIOLATION",
           } as CheckUniquenessError)
         : okAsync(),
@@ -65,28 +67,11 @@ const checkUniqueness = (values: z.infer<typeof signUpFormSchema>) =>
     );
 
 const checkUniquenessEmail = ({ email }: { email: string }) =>
-  createServiceClient()
-    .andThen((supabase) =>
-      fromSafePromise(
-        supabase.auth.admin.listUsers({
-          page: 1,
-          perPage: 300,
-        }),
-      ),
-    )
-    .andThen((response) =>
-      !response.error
-        ? okAsync(response.data.users)
-        : errAsync({
-            message: `Failed to check uniqueness`,
-            code: "DATABASE_ERROR",
-          } as CheckUniquenessError),
-    )
-    .andThen((users) =>
-      users.find((user) => user.email === email) === undefined
-        ? okAsync()
-        : errAsync({
-            message: "Email already exists.",
-            code: "UNIQUE_VIOLATION",
-          } as CheckUniquenessError),
-    );
+  runQuery((supabase) => supabase.from("users").select("*").or(`email.eq.${email}`)).andThen((data) =>
+    data.length !== 0
+      ? errAsync({
+          message: "Email already exists.",
+          code: "UNIQUE_VIOLATION",
+        } as CheckUniquenessError)
+      : okAsync(),
+  );
